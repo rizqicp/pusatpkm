2a3aa62593fd4b07dabdc86ddb98f6e0463fdcbf
Rizqi Chandra <rizqicp@gmail.com>
2020-11-28 00:54:35 +0700

    fix tambah pengajuan page

--- application/views/user/mahasiswa/tambahpengajuan.php
+++ application/views/user/mahasiswa/tambahpengajuan.php
@@ -35,88 +35,76 @@
 
                     <form action="<?= base_url('mahasiswa/tambahpengajuan'); ?>" method="post" enctype="multipart/form-data">
 
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <label class="input-group-text" for="inputGroupSelect01">Periode</label>
-                            </div>
-                            <select class="custom-select" name="periode" id="periode" value="<?= set_value('periode'); ?>">
-                                <?php foreach ($periode as $periode) : ?>
-                                    <option value="<?= $periode['id']; ?>"><?= $periode['tahun']; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                            <?= form_error('periode', '<small class="text-danger pl-3">', '</small>'); ?>
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <label class="input-group-text" for="inputGroupSelect01">Kategori</label>
-                            </div>
-                            <select class="custom-select" name="kategori" id="kategori" value="<?= set_value('kategori'); ?>">
-                                <?php foreach ($kategori as $kategori) : ?>
-                                    <option value="<?= $kategori['id']; ?>"><?= $kategori['nama']; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                            <?= form_error('kategori', '<small class="text-danger pl-3">', '</small>'); ?>
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text" id="basic-addon1">Judul</span>
-                            </div>
-                            <input type="text" class="form-control" name="judul" id="judul" value="<?= set_value('judul'); ?>">
-                            <?= form_error('judul', '<small class="text-danger pl-3">', '</small>'); ?>
-                        </div>
-
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">Abstraksi</span>
-                            </div>
-                            <textarea class="form-control" name="abstraksi" id="abstraksi" value="<?= set_value('abstraksi'); ?>" cols="30" rows="10"></textarea>
-                            <?= form_error('abstraksi', '<small class="text-danger pl-3">', '</small>'); ?>
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">Dana Ajuan</span>
-                            </div>
-                            <input type="number" class="form-control" name="dana" id="dana" value="<?= set_value('dana'); ?>" min="0" max="999999999">
-                            <?= form_error('dana', '<small class="text-danger pl-3">', '</small>'); ?>
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">NIDN Dosen Pembimbing</span>
-                            </div>
-                            <input type="text" class="form-control" name="dosenNidn" id="dosenNidn" value="<?= set_value('dosenNidn'); ?>">
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary" type="button" id="button-addon2">Cari</button>
-                            </div>
-                            <?= form_error('dosenNidn', '<small class="text-danger pl-3">', '</small>'); ?>
-                        </div>
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">NPM Anggota Tim</span>
-                            </div>
-                            <input type="text" class="form-control" name="mahasiswaNpm" id="mahasiswaNpm" value="<?= set_value('mahasiswaNpm'); ?>">
-                            <div class="input-group-append">
-                                <button class="btn btn-outline-secondary" type="button" id="button-addon2" data-toggle="modal" data-target="#tambahAnggotaModal">Tambah</button>
+                        <div class="form-group row">
+                            <label for="periode" class="col-sm-2 col-form-label col-form-label-lg"><b>Periode</b></label>
+                            <div class="col-sm-10 pt-1">
+                                <select class="custom-select form-control" id="periode" name="periode">
+                                    <?php foreach ($periode as $periode) : ?>
+                                        <option value="<?= $periode['id']; ?>"><?= $periode['tahun']; ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                                <?= form_error('periode', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                         </div>
-
-                        <div class="input-group mb-3">
-                            <div class="input-group-prepend">
-                                <span class="input-group-text">Upload</span>
-                            </div>
-                            <div class="custom-file">
-                                <input type="file" class="custom-file-input" name="userFile" id="userFile">
-                                <label class="custom-file-label" for="userFile">Choose file</label>
+                        <div class="form-group row">
+                            <label for="kategori" class="col-sm-2 col-form-label col-form-label-lg"><b>Kategori</b></label>
+                            <div class="col-sm-10 pt-1">
+                                <select class="custom-select form-control" id="kategori" name="kategori">
+                                    <?php foreach ($kategori as $kategori) : ?>
+                                        <option value="<?= $kategori['id']; ?>"><?= $kategori['nama']; ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                                <?= form_error('kategori', '<small class="text-danger pl-3">', '</small>'); ?>
                             </div>
                         </div>
-
-                        <button class="btn btn-primary mb-3" type="submit" value="upload">Buat Pengajuan</button>
-
+                        <div class="form-group row mt-5">
+                            <label for="judul" class="col-sm-2 col-form-label col-form-label-lg"><b>Judul</b></label>
+                            <div class="col-sm-10 pt-1">
+                                <input type="text" class="form-control " id="judul" name="judul" value="<?= set_value('judul'); ?>" onchange="hideError('judulError');">
+                                <?= form_error('judul', '<small id="judulError" class="text-danger pl-3">', '</small>'); ?>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="abstraksi" class="col-sm-2 col-form-label col-form-label-lg"><b>Abstraksi</b></label>
+                            <div class="col-sm-10 pt-1">
+                                <textarea class="form-control" name="abstraksi" id="abstraksi" rows="5" onchange="hideError('abstraksiError');"><?= set_value('abstraksi'); ?></textarea>
+                                <?= form_error('abstraksi', '<small id="abstraksiError" class="text-danger pl-3">', '</small>'); ?>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="dana" class="col-sm-2 col-form-label col-form-label-lg"><b>Dana Ajuan</b></label>
+                            <div class="col-sm-10 pt-1">
+                                <input type="number" class="form-control " id="dana" name="dana" value="<?= set_value('dana'); ?>" min="0" max="999999999" placeholder="0" onchange="hideError('danaError');">
+                                <?= form_error('dana', '<small id="danaError" class="text-danger pl-3">', '</small>'); ?>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="dosenNidn" class="col-sm-2 col-form-label col-form-label-lg"><b>Pembimbing</b></label>
+                            <div class="col-sm-10 pt-1">
+                                <input type="text" class="form-control " id="dosenNidn" name="dosenNidn" value="<?= set_value('dosenNidn'); ?>" onchange="hideError('dosenNidnError');">
+                                <?= form_error('dosenNidn', '<small id="dosenNidnError" class="text-danger pl-3">', '</small>'); ?>
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="mahasiswaNpm" class="col-sm-2 col-form-label col-form-label-lg"><b>Anggota</b></label>
+                            <div class="col-sm-10 pt-1">
+                                <input type="text" class="form-control " id="mahasiswaNpm" name="mahasiswaNpm" value="<?= set_value('mahasiswaNpm'); ?>" onchange="hideError('mahasiswaNpmError');">
+                                <?= form_error('mahasiswaNpm', '<small id="mahasiswaNpmError" class="text-danger pl-3">', '</small>'); ?>
+                            </div>
+                        </div>
+                        <div class="form-group row mt-5">
+                            <label for="userFile" class="col-sm-2 col-form-label col-form-label-lg"><b>Proposal</b></label>
+                            <div class="input-group mb-3 col-sm-10">
+                                <div class="custom-file">
+                                    <input type="file" class="custom-file-input" id="userFile" name="userFile" accept=".doc, .docx" onchange="validateFileType(); changeText('gambarLabel',this.value); loadImage(event);">
+                                    <label class="custom-file-label" for="userFile" id="gambarLabel" style="color: #999;">Pilih file</label>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" alue="upload" class="btn btn-primary btn-user btn-block">Buat Pengajuan</button>
+                        <div class="text-center mt-3">
+                            <a class="small" href="<?= base_url('mahasiswa/pengajuan'); ?>">&larr; Kembali &nbsp;&nbsp;</a>
+                        </div>
                     </form>
 
                 </div>
